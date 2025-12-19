279f0956d297cf19e36d917e3b37db9748896f58
E24028 <e24028tv@shibaurafzk.com>
2025-12-19 11:04:09 +0900

    Update 静カニクリッカーチート.js!!!

--- 静カニクリッカーチート.js
+++ 静カニクリッカーチート.js
@@ -1,3 +1,4 @@
+// 1
 var Element = document.createElement('div');
 document.body.appendChild(Element);
 
@@ -13,8 +14,61 @@ function Forever(n)
 
     setTimeout(Forever, 10, n + 1);
 }
-
 Forever(1);
+
+// 2
+var Element2 = document.createElement('div');
+document.body.appendChild(Element2);
+
+function Forever2(n)
+{
+    var mainImage = document.getElementById('mainImage')
+    mainImage.click();
+
+    var clickArea = document.getElementById('clickArea')
+    clickArea.click();
+
+    Element2.innerText = 'click! ' + n;
+
+    setTimeout(Forever2, 10, n + 1);
+}
+Forever2(1);
+
+// 3
+var Element3 = document.createElement('div');
+document.body.appendChild(Element3);
+
+function Forever3(n)
+{
+    var mainImage = document.getElementById('mainImage')
+    mainImage.click();
+
+    var clickArea = document.getElementById('clickArea')
+    clickArea.click();
+
+    Element3.innerText = 'click! ' + n;
+
+    setTimeout(Forever3, 10, n + 1);
+}
+Forever3(1);
+
+// 4
+var Element4 = document.createElement('div');
+document.body.appendChild(Element4);
+
+function Forever4(n)
+{
+    var mainImage = document.getElementById('mainImage')
+    mainImage.click();
+
+    var clickArea = document.getElementById('clickArea')
+    clickArea.click();
+
+    Element4.innerText = 'click! ' + n;
+
+    setTimeout(Forever4, 10, n + 1);
+}
+Forever4(1);
 
 // 埋め込みコード
 // <script src="https://E24028.github.io/静カニクリッカーチート.js"></script>
