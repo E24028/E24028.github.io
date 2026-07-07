178656ff29294542efe8a0c4aa0d7b81e145574e
E24028 <e24028tv@shibaurafzk.com>
2026-07-08 08:11:35 +0900

    Add new blog entry for chat app announcement

--- ブログエンジン.js
+++ ブログエンジン.js
@@ -1,4 +1,5 @@
 let blogData = [
     { index: 0, title: "ブログ開設してみた！", day: "2026/05/11", data: "ここに開発していて思ったことを書こうと思います。" },
-    { index: 1, title: "GASでデータサーバー設立してみる", day: "2026/05/13", data: "Google Apps Script で Google ドキュメントと紐づけてデータサーバーにできることを検証してみます。考査前なのでまだ始めませんが、6月にはできそうです。" }
+    { index: 1, title: "GASでデータサーバー設立してみる", day: "2026/05/13", data: "Google Apps Script で Google ドキュメントと紐づけてデータサーバーにできることを検証してみます。考査前なのでまだ始めませんが、6月にはできそうです。" },
+    { index: 2, title: "チャットアプリ開設のお知らせ", day: "2026/07/08", data: "チャットアプリを開設しました。<a href=&quot;https://E24028.github.io/チャットアプリ2.html&quot;>こちらから</a>" }
 ];
